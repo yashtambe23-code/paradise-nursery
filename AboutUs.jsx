ef89d83015af3cfa566bdfceefb6d69c1271ef82
yashtambe23-code <yash.tambe23@vit.edu>
2026-04-15 22:39:19 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery was founded in 2020 with a mission to bring nature into every home.
+        We offer a wide range of healthy, beautiful indoor and outdoor plants.
+      </p>
+      <p>
+        Our company believes in sustainable gardening and providing expert advice to help
+        your plants thrive. Located in the heart of the city, we serve thousands of happy customers.
+      </p>
+      <h3>Contact</h3>
+      <p>Email: hello@paradisenursery.com</p>
+      <p>Phone: +1 (555) 123-4567</p>
+    </div>
+  );
+};
+
+export default AboutUs;
